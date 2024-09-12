048ff7a0fb5cb8677dc4e48b3b2a8c21f01f8e92
ChernHwee <chernhwee_93@hotmail.com>
2024-09-12 12:44:17 +0800

    Remove comments and add back flipHorizontal and flipVertical

--- App.js
+++ App.js
@@ -53,22 +53,15 @@ export default function App() {
 
     const flipHorizontal = Math.floor(Math.random() * 2);
     const flipvertical = Math.floor(Math.random() * 2);
-    console.log('===========================')
     if (rotate > 4) {
       defaultPositionArray = generatingPosition(rotate - 4, rightRotation, [...defaultPositionArray])
       console.log(`rotateRight: ${rotate - 4}`);
     }
-    if (rotate < 3) {
+    else if (rotate < 3) {
       defaultPositionArray = generatingPosition(rotate + 1, leftRotation, [...defaultPositionArray])
-      console.log(`rotateLeft: ${rotate + 1}`);
     }
-    else {
-      console.log("don't rotate");
-    }
-    // defaultPositionArray = generatingPosition(flipHorizontal, horizontalFlipped, [...defaultPositionArray])
-    // console.log(`flipHorizontal: ${flipHorizontal}`);
-    // defaultPositionArray = generatingPosition(flipvertical, vertialFlipped, [...defaultPositionArray])
-    // console.log(`flipvertical: ${flipvertical}`);
+    defaultPositionArray = generatingPosition(flipHorizontal, horizontalFlipped, [...defaultPositionArray])
+    defaultPositionArray = generatingPosition(flipvertical, vertialFlipped, [...defaultPositionArray])
   }
 
   function randomNumber(values) {
